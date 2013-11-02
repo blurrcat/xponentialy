bfffee092da26ec3d853f18c9f320f75b4713a9c
blurrcat <blurrcat@gmail.com>
2013-11-02 11:33:05 +0800

    Add base html templates

--- xponentialy/views/__init__.py
+++ xponentialy/views/__init__.py
@@ -1,11 +1,14 @@
-from flask import Blueprint
-
-from xponentialy.auth import auth
+from flask import Blueprint, url_for
+from flask import request
+from flask import redirect
+from flask import render_template
 
 import admin
 import api
 import leaderborad
 from .fitbit import fitbit_bp
+from xponentialy.auth import auth
+
 
 xp_bp = Blueprint('xponentialy', 'xponentialy')
 
@@ -13,4 +16,13 @@ xp_bp = Blueprint('xponentialy', 'xponentialy')
 @xp_bp.route('/', methods=['GET'])
 @auth.login_required
 def index():
-    return 'Home sweet home'
+    return render_template('index.html')
+
+
+@xp_bp.route('/user_info', methods=['POST', 'GET'])
+@auth.login_required
+def user_info():
+    if request.method == 'GET':
+        return 'user info form here'
+    else:
+        return redirect(url_for('.index'))
